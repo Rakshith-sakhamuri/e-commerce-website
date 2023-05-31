8ec0b579aedff3662c282f37d4887cbf041fa4c3
rakshith <sakhamurirakshith@gmail.com>
2023-05-31 20:23:36 +0530

    Updated products filer

--- frontend/src/pages/ProductList.js
+++ frontend/src/pages/ProductList.js
@@ -6,27 +6,42 @@ import PriceFilterComponent from "../components/filterQueryResultOptions/PriceFi
 import RatingFilterComponent from "../components/filterQueryResultOptions/RatingFilterComponent";
 import CategoryFilterComponent from "../components/filterQueryResultOptions/CategoryFilterComponent";
 import AttributesFilterComponent from "../components/filterQueryResultOptions/AttributesFilterComponent";
-const ProductList = () => {
+const ProductListPage = () => {
   return (
     <Container fluid>
       <Row>
         <Col md={3}>
           <ListGroup variant="flush">
-            <ListGroup.Item><SortOptionsComponent /></ListGroup.Item>
-            <ListGroup.Item><PriceFilterComponent /></ListGroup.Item>
-            <ListGroup.Item><RatingFilterComponent /></ListGroup.Item>
-            <ListGroup.Item><CategoryFilterComponent /></ListGroup.Item>
+            <ListGroup.Item className="mb-3 mt-3">
+              <SortOptionsComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              FILTER: <br />
+              <PriceFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <RatingFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <CategoryFilterComponent />
+            </ListGroup.Item>
             <ListGroup.Item>
               <AttributesFilterComponent />
             </ListGroup.Item>
             <ListGroup.Item>
-              <Button variant="primary">Primary</Button>
-              <Button variant="danger">Danger</Button>
+              <Button variant="primary">Filter</Button>
+              <Button variant="danger">Reset filters</Button>
             </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={9}>
-          <ProductForListComponent />
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <ProductForListComponent
+              key={idx}
+              images={["games", "monitors", "tablets", "games", "monitors"]}
+              idx={idx}
+            />
+          ))}
           <PaginationComponent />
         </Col>
       </Row>
@@ -34,4 +49,4 @@ const ProductList = () => {
   );
 };
 
-export default ProductList;
+export default ProductListPage;
